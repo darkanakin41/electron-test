9dc417bbc4940fb666f6623f96f4eaf7db972359
Pierre LEJEUNE <darkanakin41@gmail.com>
2020-03-22 19:39:55 +0100

    first commit

--- README.md
+++ README.md
@@ -22,3 +22,5 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+build using https://www.cseyda.de/2019/04/21/how-to-start-an-electron-vue-typescript-project/
---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-footer app height="auto">
+        <p class="copyright">
+            <i class="far fa-copyright"></i> Copyright Pierre LEJEUNE, tous droits réservés.
+        </p>
+        <div class="social">
+            <a href="https://github.com/darkanakin41/" target="_blank" class="gitlab"><i class="fab fa-github"></i></a>
+            <a href="https://twitter.com/dark_csgo" target="_blank" class="twitter"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/pierre-lejeune/" target="_blank" class="linkedin"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+    </v-footer>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component({
+        components: {}
+    })
+    export default class TopBarContent extends Vue {
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
